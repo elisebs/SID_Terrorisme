33600825bbecd293c90cf4f500cf346153be4b3d
Elise Benois <benoiselise@gmail.com>
2018-02-16 11:32:36 +0100

    -> à essayer de mettre dans path_target

--- filtrage.py
+++ filtrage.py
@@ -13,6 +13,8 @@ import pickle
 nlp = spacy.load('fr')
 from tqdm import tqdm
 
+path_target = '/Users/Elise/SID_Terrorisme/path_target'
+
 #stop_words = pickle.load(open('/var/www/html/projet2018/code/filtering/' + 'functions/stopwords.p', 'rb'))
 stop_words = pickle.load(open('/Users/Elise/SID_Terrorisme/stopwords.p', 'rb'))
 
@@ -348,7 +350,7 @@ def tokeniz(text):  # Tokenize a text with library Spacy
     return doc
 
 
-# b = tokeniz(clean2Text[0])
+# b = tokeniz(clean_symbols[0])
 
 # b[8] renvoie le 8e mot
 
@@ -464,40 +466,11 @@ def tag_text(article, is_title=False):
     return analys_token(article, tokens, entity_, is_title=is_title)
 
 
-test = tag_text(docs, is_title=False)
+test = tag_text(docs, is_title=True)
 print(test)
 
-with tqdm(desc='JSONing', total=len(docs)) as pbar:
-    tableau_vide = []
-    for item in docs:
-        art = docs[item]
-        data_post_content, filtered = tag_text(art, isTitle=False)
+data_post_content, filtered = tag_text(docs, is_title=False)
+data_post_title = tag_text(docs, is_title=True)
+data_post_title = list(data_post_title)
 
-        data_post_title = tag_text(art, isTitle=True)
-        data_post_title = list(data_post_title)
-        for dic in range(len(data_post_title)):
-            data_post_content["position_word"].append(data_post_title[dic])
-        data_post_content["id_art"] = art["id_art"]
-        data_post = []
-        data_post.append(data_post_content)
-#        data_post = json.dumps(data_post, ensure_ascii='False')
-#        print('POST filtering en cours ...')
-#        log_post_filt = post_filtering(data_post)
-#        id_article = log_post_filt.json()[0][0]["message"]["id_article"]
-        ifile = path_post_filt_target + '/' + item + '_post_filtered.json'
-        with open(ifile, 'w', encoding='utf-8') as outfile:
-            json.dump(data_post, outfile, ensure_ascii=False)
-
-        tfidf = get_tf_idf(filtered['list_lemma'], art["id_art"])
-#        tfidf = json.dumps(tfidf, ensure_ascii='False')
-#        print('POST TF en cours ...')
-#        log_post_tf = post_tfidf(tfidf)
-#        print('POST TF OK')
-#        print('log_post_tf = '+str(log_post_tf))
-        ifile = path_post_tf_target + '/' + item + '_post_tf.json'
-        with open(ifile, 'w', encoding='utf-8') as outfile:
-            json.dump(tfidf, outfile, ensure_ascii=False)
-        pbar.update()
-        
-import spacy
-x=spacy.load('en')
+ifile = path_target + '/' + str(filtered) + '_filtering.json'
